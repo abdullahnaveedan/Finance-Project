5905fbd8631b45df84e3b12980cd2644d55ed998
Abdullah Naveed <naveedabdullah00001@gmail.com>
2024-01-18 17:29:24 +0500

    Finance Project

--- cmsfinanceapp/urls.py
+++ cmsfinanceapp/urls.py
@@ -12,13 +12,7 @@ urlpatterns = [
     path('sign-up/',views.sign_up,name="sign_up"),#working
     path('sign-out/',views.sign_out,name="sign_out"),#working
     path("forget_password/", views.forget_password, name="forget_password"),
-<<<<<<< HEAD
     path("otp/", views.otp_validation, name="otp_validation"),
     path("reset_password/", views.reset_password, name="reset_password"),
     path("table-data/", views.get_table_data, name="")
-=======
-    path('show_forget_password/',views.show_forget_password,name="show_forget_password"),#working
-    path("otp_validation/", views.otp_validation, name="otp_validation"),
-    path("reset_password/", views.reset_password, name="reset_password")
->>>>>>> 1dc64f7c3724c683c63729aa64ea27653a136e09
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
