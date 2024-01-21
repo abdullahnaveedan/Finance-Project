4b13805bdc415478bd41f582e645d05489bff05f
Abdullah Naveed <128359566+abdullahnaveedan@users.noreply.github.com>
2024-01-21 18:33:24 +0500

    Update views.py

--- cmsfinanceapp/views.py
+++ cmsfinanceapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse 
+from django.http import HttpResponse
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.models import User
@@ -9,24 +9,23 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .utils import generate_otp, send_otp_email
 import os
-import pandas as pd 
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
-import seaborn as sns
 from collections import OrderedDict
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+# from django_pandas.io import read_frame
 
 
-def get_table_data(file_path):
-    
+def get_table_data(file_path, dict_to_map):
+
     try:
         data = pd.read_excel(file_path)
     except Exception as e:
         return f"Error processing the file: {str(e)}"
-
+    data = data.rename(columns = dict_to_map)
     data['disbursement_year'] = data['Disbursement Date'].dt.year
     data['last_payment_year'] = data['Date of Last Payment'].dt.year
 
@@ -47,7 +46,7 @@ def get_table_data(file_path):
 
 
           # Calculating the difference between 'VALOR_DESEMBOLSOS' and 'BALANCE_CAPITAL'
-         
+
     data['Difference_disbursement_capitalBalance'] = data['Disbursement Amount'] - data['Capital Balance']
     # Grouping this difference by year of 'FECHA_DESEMBOLSO'
     grouped_difference_by_year = data.groupby('disbursement_year')['Difference_disbursement_capitalBalance'].sum().reset_index()
@@ -64,36 +63,40 @@ def get_table_data(file_path):
     grouped_data_with_values['total_difference_disbursement_capitalBalance'] / grouped_data_with_values['Total_disbursement_amount']) * 100
     grouped_data_with_values['Cumulative_Percentage_Difference'] = grouped_data_with_values['Percentage_Difference'].cumsum()
     group = grouped_data_with_values.values.tolist()
-            
-    
+
+
     return group
 # Create your views here.
-
-@login_required(login_url='sign_in') 
+@login_required(login_url='sign_in')
 def index(request):
     data = file_data.objects.filter(username = request.user).order_by('-id')
     latest_record = data.first()
     file = latest_record.excel_file
+    file_path = os.path.join(settings.MEDIA_ROOT, str(file))
+    dict_to_map = {'CODIGO_CLIENTE': "Customer Code", 'FECHA_DESEMBOLSO': "Disbursement Date", 'VALOR_DESEMBOLSOS':'Disbursement Amount',
+       'VALOR_CUOTA':'Installment Value', 'DIAS_ATRASO_CAPITAL': 'Days of Capital Delay', 'MESES_EN_ATRASO': 'Months in Arrears', 'SALDO_ACTUAL':'Current Balance',
+       'BALANCE_CAPITAL': 'Capital Balance', 'INTERES': 'Interest', 'MORA': 'Late Payment Interest / Arrears', 'FECHA_ULTIMO_PAGO': 'Date of Last Payment',
+       'MONTO_ULTIMO_PAGO': 'Amount of Last Payment', 'FECHA_CASTIGO': 'Write-Off Date'}
 
-    file_path = 'media/' + str(file)
-
+    group = ''
     try:
         data = pd.read_excel(file_path)
-        group = get_table_data(file_path)
+        group = get_table_data(file_path, dict_to_map)
     except Exception as e:
-        return f"Error processing the file: {str(e)}"
-    
+        print("Error processing the file.")
+    data = pd.read_excel(file_path)
+    data = data.rename(columns = dict_to_map)
     cols_drop=['Customer Code','Disbursement Date','Date of Last Payment','Write-Off Date']
     filtered_data = data.drop(columns=cols_drop)
     mean = filtered_data.mean()
     toMean = mean.to_dict()
-    
+
     #Total rows in a dataset
     dataset_len = len(data)
-    
+
     #Total unique rows in a dataset
     unique_customers = data['Customer Code'].nunique()
-    
+
     # This data is for double bar plot of Write off count and total disbursed count by year
     # Create a new column for the write-off year
     data['write_off_year'] = data['Write-Off Date'].dt.year
@@ -102,7 +105,7 @@ def index(request):
     write_off_count_per_year = data.groupby('write_off_year')['Write-Off Date'].count()
     write_off_index = list(write_off_count_per_year.index)
     write_off_values = list(write_off_count_per_year.values)
-    
+
 
     # Calculate yearly loan performance
     data['Year'] = data['Disbursement Date'].dt.year
@@ -125,11 +128,11 @@ def index(request):
 
     # Analysis of Capital Balance over Time
     #This data is for Average Capital Balance Over Time
-    data['Year of Last Payment'] = data['Date of Last Payment'].dt.year 
+    data['Year of Last Payment'] = data['Date of Last Payment'].dt.year
     capital_balance_over_time = data.groupby('Year of Last Payment')['Capital Balance'].mean()
     capitalBalanceIndex = list(capital_balance_over_time.index)
     capitalBalanceValues= list(np.round_(capital_balance_over_time.values))
-    
+
 
     # Distribution of Months in Arrears
     # This data is for plot of Distribution of Months in Arrears
@@ -143,7 +146,7 @@ def index(request):
     data['Loan Age'] = round((pd.to_datetime('2024-01-01') - data['Disbursement Date']).dt.days / 365.25)
     loan_age_distribution = dict(OrderedDict(sorted(data['Loan Age'].value_counts().to_dict().items())))
     loanAgeDistribution_x = list(loan_age_distribution.keys())
-    loanAgeDistribution_y = list(loan_age_distribution.values())    
+    loanAgeDistribution_y = list(loan_age_distribution.values())
     # This data is for pie plot of Loan Size Category
     #Loan Size Category
     data['Loan Size Category'] = pd.cut(data['Disbursement Amount'], bins=[0, 100000, 500000, 1000000], labels=["Small (0 - 100K)", "Medium (100K - 500K)", "Large (500K - 1M)"])
@@ -152,7 +155,7 @@ def index(request):
     proportion_by_loan_size = data['Loan Size Category'].value_counts(normalize=True)
     loanSizeIndex = list(proportion_by_loan_size.index)
     loanSizeValues = list(np.round_(proportion_by_loan_size.values*100 , decimals = 3))
-    
+
     y_index = sorted(set(disbursed_index) | set(write_off_index))
 
     # Initialize a-y and b-y with zeros
@@ -168,9 +171,9 @@ def index(request):
     for i, x_value in enumerate(write_off_index):
         index = y_index.index(x_value)
         write_off_values_updated[index] = write_off_values[i]
-    
+
     # Read and send excel file
- 
+
     context = {
              'dataset_mean' : toMean, # Done Speedometer
              'dataset_length':dataset_len, # Done mini-1
@@ -182,9 +185,9 @@ def index(request):
              'loan_age_distribution':{'x': loanAgeDistribution_x, 'y':loanAgeDistribution_y}, # Done Line bar
              'proportion_by_loan_size': {'x':[loanSizeIndex],'y':[loanSizeValues]} ,# Done PIE
              'group' : group
-            }                       
-    return render(request, "dashboard.html",context )
-    
+            }
+    return render(request, "dashboard.html",context)
+
 def sign_in(request):
     if request.method == "POST":
             username = request.POST.get("username")
@@ -201,14 +204,14 @@ def sign_in(request):
                 return render(request, "upload_file.html")
             else:
                 messages.warning(request, 'Wrong credentials.')
-        
+
     return render(request, "sign_in.html")
 
 def sign_out(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect("sign_in")
-        
+
 
 #validate user's data provided by sing_up function
 def validate_data(first_name, last_name, username, email, password, confirm_password):
@@ -290,7 +293,7 @@ def otp_validation(request):
                 # Incorrect OTP, redirect to forget_password.html
                 messages.error(request, "Incorrect OTP. Please try again.")
                 return render(request, "otp.html")
-            
+
         elif context == 'reset_password':
             # Retrieve forget password data from the session
             reset_password_data = request.session.get('reset_password')
@@ -321,7 +324,6 @@ def sign_up(request):
         validation_result, validation_message = validate_data(
             getFirstName, getLastName, getUserName, getEmail, getPassword, getConfirmPassword
         )
-        print(validation_message)
         if not validation_result:
             # If validation fails, display an error message
             messages.error(request, validation_message)
@@ -395,7 +397,7 @@ def reset_password(request):
     return render(request, "reset_password.html")
 
 def upload_file(request):
-   
+
     return render(request, "upload_file.html")
 
 def submit_excel(request):
@@ -413,13 +415,13 @@ def submit_excel(request):
     return redirect("upload-file")
 
 def show_sign_in(request):
-    
+
     return render(request, "sign_in.html")
 
 def show_sign_up(request):
-    
+
     return render(request, "sign_up.html")
 
 def show_forget_password(request):
-    
+
     return render(request, "forget_password.html")
